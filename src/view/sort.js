32bfabb49c97f5e2f6641b53348493315bdd95d0
Azarak <abbatd@gmail.com>
2021-08-26 17:51:28 +1000

    Рефактор компонента сортировки

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,10 +1,20 @@
 ﻿import AbstractView from './abstract.js';
 
+const sortTypes = [
+  'default',
+  'date',
+  'rating',
+];
+
+const createSortButton = (sortType) => (
+  `<li><a href="#" class="sort__button" data-sort-type="${sortType}">Sort by ${sortType}</a></li>`
+);
+
+// sort__button--active
+
 const createSortTemplate = () => (
   `<ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button">Sort by rating</a></li>
+    ${sortTypes.map((sortType) => createSortButton(sortType)).join(' ')}
   </ul>`
 );
 
