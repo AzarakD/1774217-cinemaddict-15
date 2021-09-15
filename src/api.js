4d741cf359616ac05869f8c0e222867cf09f6edc
Azarak <abbatd@gmail.com>
2021-09-15 17:40:17 +1000

    Добавил класс api

---
+++ src/api.js
@@ -0,0 +1,61 @@
+﻿// https://15.ecmascript.pages.academy/cinemaddict
+// Basic BBziUcTOvPtL7Qy <- 15 chars
+
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+};
+
+export default class Api {
+  constructor(endPoint, authorization) {
+    this._endPoint = endPoint;
+    this._authorization = authorization;
+  }
+
+  getFilms() {
+    return this._load({url: 'movies'})
+      .then(Api.toJSON);
+  }
+
+  updateFilm(film) {
+    return this._load({
+      url: `movies/${film.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(film),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    })
+      .then(Api.toJSON);
+  }
+
+  _load({
+    url,
+    method = Method.GET,
+    body = null,
+    headers = new Headers(),
+  }) {
+    headers.append('Authorization', this._authorization);
+
+    return fetch(
+      `${this._endPoint}/${url}`,
+      {method, body, headers},
+    )
+      .then(Api.checkStatus)
+      .catch(Api.catchError);
+  }
+
+  static toJSON(response) {
+    return response.json();
+  }
+
+  static checkStatus(response) {
+    if (!response.ok) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+
+    return response;
+  }
+
+  static catchError(error) {
+    throw error;
+  }
+}
